6b11814fbd3165c0aa0594c8246e730728c9e27c
Cody Stoltman <particlebanana@gmail.com>
2016-10-18 12:04:42 -0500

    fix port

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.send('Logged some characters.');
 });
 
-var PORT = process.env.port || 3000;
+var PORT = process.env.PORT || 3000;
 
 app.listen(PORT, function () {
   console.log('Example app listening on port ' + PORT + '!');
